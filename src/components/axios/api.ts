fa5acb0766e85f57aa5c2b660d7d53ced3d8cebf
Kauã Victor <kauavictor.dev@gmail.com>
2024-06-16 15:09:35 -0300

    a

--- src/components/axios/api.ts
+++ src/components/axios/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-    baseURL: 'https://api.jsonbin.io/v3/b/666e5a88acd3cb34a8581998',
+    baseURL: 'https://api.jsonbin.io/v3/b/666f29ceacd3cb34a85858dc',
     headers: {
         'Content-Type': 'application/json',
         'X-Master-Key': '$2a$10$Jc30FQOuDFt02TH7CX9SDutiyMRjzAREoc5xKg.MMXxsu.QBNbonm'
