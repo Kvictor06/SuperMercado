f88616208e221c4244297083976850686e164c6e
Kauã Victor <kauavictor.dev@gmail.com>
2024-06-16 00:17:14 -0300

    t

--- src/main.tsx
+++ src/main.tsx
@@ -1,31 +1,29 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
-import { BrowserRouter as Router, createBrowserRouter, RouterProvider } from 'react-router-dom'
-import Home from './components/pages/Home.tsx/Home.tsx'
-import Debts from './components/pages/debts/Debts.tsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./components/pages/Home.tsx/Home.tsx";
+import Debts from "./components/pages/debts/Debts.tsx";
 
 const router = createBrowserRouter([
   {
-    path: '/',
+    path: "/",
     element: <App />,
     children: [
       {
-        path: 'home',
-        element: <Home/>
+        path: "home",
+        element: <Home />,
       },
-      { path: 'debts',
-        element: <Debts/>
-      }
-    ]
-  }
-  ])
+      { path: "debts", element: <Debts /> },
+    ],
+  },
+],
+{basename: '/SuperMercado'}
+);
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <Router>
-        <RouterProvider router={router}/>
-    </Router>
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
